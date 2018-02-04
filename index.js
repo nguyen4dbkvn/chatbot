50a980e3dd1b9af015413a526722cc5710bc8d36
NBNguyen <nbnguyen@unisrv.jp>
2018-02-04 21:26:38 +0900

    test auto comment

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const
    express = require('express'),
    request = require('request'),
    bodyParser = require('body-parser'),
+   fptAI = require('./fpt-ai'),
    app = express().use(bodyParser.json());
 
 let VERIFY_TOKEN = "EAAEOCh2yDjwBAKLdOw21Rf132ck5V7jsWLiTHxZBBj9u4b5aH8BTmHJdMXg2UW3VjkxiMJvovpWWwipMSDsVrgMn4o9Qe3hVKP8p2F1RjVxii1F2lgNOAAE6ZAQJo7QIZAIq2zZCUZA15qeouBIbRCths4HspgK3e35wrXh2lZBjMNDuIZAvyaU";
@@ -11,7 +12,17 @@ let VERIFY_TOKEN = "EAAEOCh2yDjwBAKLdOw21Rf132ck5V7jsWLiTHxZBBj9u4b5aH8BTmHJdMXg
 app.listen(process.env.PORT || 4000, () => console.log('webhook is listening.'));
 
 app.get('/', (req, res) => {
-	res.status(200).send('Geted!');
+   var callback = function (err, res, body) {
+      if (err) {
+         console.error(err);
+      }
+      else {
+         console.log(body);
+      }
+   }
+   var content = req.query['content'];
+   fptAI.getDateTime(content, callback);
+   res.status(200).send('OK');
 });
 
 app.post('/webhook', (req, res) => {
@@ -19,20 +30,28 @@ app.post('/webhook', (req, res) => {
 
    if (body.object === 'page') {
       body.entry.forEach(function (entry) {
-         entry.messaging.forEach(function (webhookEvent) {
-			 let senderId = webhookEvent.sender.id;
-			 let recipientId = webhookEvent.recipient.id;
-			 let timestamp = webhookEvent.timestamp;
-			 console.log('\n---------------------');
-			 console.log(webhookEvent);
-			 if (webhookEvent.message) {
-				 let msgText = webhookEvent.message.text;
-				 console.log("msgText: ", msgText);
-				sendReplyMessage(recipientId, senderId, msgText);
-			 }
-			 console.log('---------------------\n');
-		 });
-		 
+         if (entry.hasOwnProperty('changes')) {
+            entry.changes.forEach(function (changes) {
+               // if (changes.field === 'feed' && changes.value.item)
+               console.log(changes);
+            });
+         }
+
+         if (entry.hasOwnProperty('messaging') {
+            entry.messaging.forEach(function (webhookEvent) {
+               let senderId = webhookEvent.sender.id;
+               let recipientId = webhookEvent.recipient.id;
+               let timestamp = webhookEvent.timestamp;
+               console.log('\n---------------------');
+               console.log(webhookEvent);
+               if (webhookEvent.message) {
+                  let msgText = webhookEvent.message.text;
+                  console.log("msgText: ", msgText);
+                  sendReplyMessage(recipientId, senderId, msgText);
+               }
+               console.log('---------------------\n');
+            });
+         }
       });
 
       res.status(200).send('OK');
@@ -77,7 +96,7 @@ function sendReplyMessage(senderId, recipientId, receivedMsg) {
 				id: recipientId
 			},
 			message: {
-				text: "[Bot] " + receivedMsg 
+				text: "[Bot] " + receivedMsg
 			}
 		}
 	});
